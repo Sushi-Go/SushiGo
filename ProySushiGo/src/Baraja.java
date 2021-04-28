afb3bc18166530c6525383f69af2218afcdeb553
Daviiiiid123 <61392628+Daviiiiid123@users.noreply.github.com>
2021-04-28 15:17:35 +0200

    Add files via upload

---
+++ ProySushiGo/src/Baraja.java
@@ -0,0 +1,16 @@
+
+import java.util.Stack;
+import pila.*;
+
+public class Baraja {
+
+    private Pila<Carta> baraja;
+    private int numCartas;
+
+    public Baraja() {
+        numCartas = 0;
+        baraja = new Pila<>;
+        
+    }
+
+}
