9ba9ad934eadf48c066824c72c871e574f2e2508
Lookos <78797521+LookosZero@users.noreply.github.com>
2021-04-28 13:47:58 +0200

    Add files via upload

--- SushiGo/src/sushigo/SushiGo.java
+++ SushiGo/src/sushigo/SushiGo.java
@@ -4,11 +4,8 @@ public class SushiGo {
 
     public static void main(String[] args) {
         
-        
-        
-        
-        
-        
+        System.out.println("Hello world!");
+
     }
     
 }
