d878801f71d278f56579d4f2230b00e4f18ee8df
Lookos <78797521+LookosZero@users.noreply.github.com>
2021-05-01 13:29:38 +0200

    Add files via upload

--- ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Carta.java
+++ ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Carta.java
@@ -5,6 +5,19 @@ package com.uvigo.poyectosushigo.CORE;
 
 
 public class Carta {
-
+    private String nombre;
+    
+    public Carta(String nombre){
+        this.nombre=nombre;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    @Override
+    public String toString(){
+        return "Carta: "+getNombre();
+    }
 	
 }
