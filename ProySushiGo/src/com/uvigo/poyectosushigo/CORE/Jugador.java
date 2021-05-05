81dbeef9fc746d4c694eeb8037976a8524961bb7
Daviiiiid123 <61392628+Daviiiiid123@users.noreply.github.com>
2021-05-05 18:50:50 +0200

    Update Jugador.java

--- ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Jugador.java
+++ ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Jugador.java
@@ -8,10 +8,8 @@ package com.uvigo.poyectosushigo.CORE;
 
 import com.uvigo.poyectosushigo.CORE.carta.Carta;
 
-
-
-
 public class Jugador {
+
     private String nombre;
     private Mano mano;
     private int puntosAcumulados;
@@ -20,12 +18,41 @@ public class Jugador {
     public Jugador(String nombre) {
         this.nombre = nombre;
     }
-    
-    
-    
-    
-    public void elegirCarta(Carta c){
-        
+
+    public String getNombre() {
+        return nombre;
     }
-	
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Mano getMano() {
+        return mano;
+    }
+
+    public void setMano(Mano mano) {
+        this.mano = mano;
+    }
+
+    public int getPuntosAcumulados() {
+        return puntosAcumulados;
+    }
+
+    public void setPuntosAcumulados(int puntosAcumulados) {
+        this.puntosAcumulados = puntosAcumulados;
+    }
+
+    public int getPuntosRondaActual() {
+        return puntosRondaActual;
+    }
+
+    public void setPuntosRondaActual(int puntosRondaActual) {
+        this.puntosRondaActual = puntosRondaActual;
+    }
+
+    public void elegirCarta(Carta c) {
+
+    }
+
 }
