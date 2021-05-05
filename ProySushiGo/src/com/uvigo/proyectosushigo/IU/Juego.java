bb742f909c93a25d7c6aad256744da56637a9a97
JesusDiaguez <83415657+JesusDiaguez@users.noreply.github.com>
2021-05-05 21:20:34 +0200

    Update Juego.java

--- ProySushiGo/src/com/uvigo/proyectosushigo/IU/Juego.java
+++ ProySushiGo/src/com/uvigo/proyectosushigo/IU/Juego.java
@@ -14,31 +14,52 @@ public class Juego {
     private int rondaActual;
 
     public static void inicio() {
-        
+
         //Texto de introducción, título, etc
         System.out.println("");
-        
-        //Obtenemos el número de jugadores y creamos un arrray
+
+        //Obtenemos el número de jugadores y sus nombres
         System.out.println("El mínimo de jugadores es 2, y el máximo 5");
         jugadores = new Jugador[pideNumJugadores()];
-        
-        
-        
-        
+        leeJugadores();
+
     }
 
     /**
      * Pide el número de jugadores por teclado y lo devuelve
-     * 
+     *
      * @return el número de jugadores, como entero
      */
     public static int pideNumJugadores() {
         int toret = pideEntero("Número de jugadores: ");
-        
+
         while (toret < 2 || toret > 5) {
             toret = pideEntero("Número de jugadores: ");
         }
         return toret;
+    }
+
+    /**
+     * Crea los jugadores leyendo los nombres de teclado
+     */
+    public static void leeJugadores() {
+        System.out.println("Introduce los nombres de cada jugador");
+        for (int i = 0; i < jugadores.length; i++) {
+            String nombre = pideCadena("Jugador " + (i + 1));
+            jugadores[i] = new Jugador(nombre);
+        }
+    }
+
+    /**
+     * Muestra por pantalla el estado actual de la mesa
+     */
+    public static void mostrarMesa() {
+        System.out.println("Estado actual de la mesa\n");
+
+        for (Jugador j : jugadores) {
+            System.out.println(j.getNombre());
+            //
+        }
     }
 
     public int getRondaActual() {
