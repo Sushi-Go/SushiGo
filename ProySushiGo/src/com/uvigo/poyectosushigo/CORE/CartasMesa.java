62ee9a0ac63b4c90716f2712d05d08a731c75922
Lookos <78797521+LookosZero@users.noreply.github.com>
2021-05-05 20:45:56 +0200

    Update CartasMesa.java

--- ProySushiGo/src/com/uvigo/poyectosushigo/CORE/CartasMesa.java
+++ ProySushiGo/src/com/uvigo/poyectosushigo/CORE/CartasMesa.java
@@ -7,9 +7,10 @@ package com.uvigo.poyectosushigo.CORE;
 
 import com.uvigo.poyectosushigo.CORE.carta.*;
 import lista.*;
+import pila.*;
 
 public class CartasMesa {
-    private Lista<Carta> cartasMesa;
+    private Lista<Baraja> cartasMesa;
     private int numCartasMesa;
     
     public CartasMesa(){
@@ -17,15 +18,30 @@ public class CartasMesa {
         cartasMesa=new ListaEnlazada<>();
     }
     
-    public void ponerSobreMesa(Carta c){
-        cartasMesa.insertarPrincipio(c);
+    public void ponerSobreMesa(Baraja b,Carta c){
+        
     }
     
-    public void retirarCarta(Carta c){
-        cartasMesa.suprimir(c);
+    public void retirarCartaMesa(Carta c){
+
+    }
+    
+    public int calcularPuntuacion(){
+        
+    }
+    
+    public void limpiarFinalRonda(){
+
+        
     }
     
     
+    public int calcularNumRollitos(){
+        
+    }
     
+    public void visualizarMesa(){
+        
+    }
 
 }
