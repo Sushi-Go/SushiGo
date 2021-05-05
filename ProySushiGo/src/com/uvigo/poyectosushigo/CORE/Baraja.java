cb4fd175ce726cd3f4b18f1aa814a12e1580da5d
Daviiiiid123 <61392628+Daviiiiid123@users.noreply.github.com>
2021-05-05 10:05:04 +0200

    :+1:

--- ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Baraja.java
+++ ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Baraja.java
@@ -1,7 +1,7 @@
-package a5e1;
-
+package com.uvigo.poyectosushigo.CORE;
 
 import pila.*;
+import com.uvigo.poyectosushigo.CORE.carta.*;
 
 public class Baraja {
 
@@ -12,55 +12,56 @@ public class Baraja {
         numCartas = 0;
         baraja = new EnlazadaPila<>();
         //Creamos las 5 carta de nigiri de calamar
-        for(int i=1; i<=5;i++){
-            baraja.push(new Carta("Nigiri de calamar"));
+        for (int i = 1; i <= 5; i++) {
+            baraja.push(new NigiriCalamar());
             numCartas++;
         }
-         //Creamos las 5 carta de nigiri de salmon
-        for(int i=1; i<=10;i++){
-            baraja.push(new Carta("Nigiri de salmón"));
+        //Creamos las 5 carta de nigiri de salmon
+        for (int i = 1; i <= 10; i++) {
+            baraja.push(new NigiriSalmon());
             numCartas++;
         }
-        for(int i=1; i<=5;i++){
-            baraja.push(new Carta("Nigiri de tortilla"));
+        for (int i = 1; i <= 5; i++) {
+            baraja.push(new NigiriTortilla());
             numCartas++;
         }
-        for(int i=1; i<=6;i++){
-            baraja.push(new Carta("Wasabi"));
+        for (int i = 1; i <= 6; i++) {
+            baraja.push(new Wasabi());
             numCartas++;
         }
-        for(int i=1; i<=6;i++){
-            baraja.push(new Carta("Maki 1 Rollo"));
+        for (int i = 1; i <= 6; i++) {
+            baraja.push(new Maki(1));
             numCartas++;
         }
-        for(int i=1; i<=12;i++){
-            baraja.push(new Carta("Maki 2 Rollo"));
+        for (int i = 1; i <= 12; i++) {
+            baraja.push(new Maki(2));
             numCartas++;
         }
-        for(int i=1; i<=8;i++){
-            baraja.push(new Carta("Maki 3 Rollo"));
+        for (int i = 1; i <= 8; i++) {
+            baraja.push(new Maki(3));
             numCartas++;
         }
-        for(int i=1; i<=14;i++){
-            baraja.push(new Carta("Tempura"));
+        for (int i = 1; i <= 14; i++) {
+            baraja.push(new Tempura());
             numCartas++;
         }
-        for(int i=1; i<=14;i++){
-            baraja.push(new Carta("Sashimi"));
+        for (int i = 1; i <= 14; i++) {
+            baraja.push(new Sashimi());
             numCartas++;
         }
-        for(int i=1; i<=14;i++){
-            baraja.push(new Carta("Gyoza"));
+        for (int i = 1; i <= 14; i++) {
+            baraja.push(new Gyoza());
             numCartas++;
         }
     }
-    private void barajar(){
-        
+
+    private void barajar() {
+
     }
-    
-    public Carta darCarta(){
-        return baraja.pop(); 
-    //DEvuelve la primera carta en el mazo
+
+    public Carta darCarta() {
+        return baraja.pop();
+        //DEvuelve la primera carta en el mazo
     }
 
 }
--- ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Mano.java
+++ ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Mano.java
@@ -2,13 +2,17 @@
 * Representa las cartas que tiene un jugador en la mano (las que dispone para jugar).
 * Estructura: Se utilizarán TAD adecuado. 
 * Funcionalidad: añadir carta a la mano, quitar carta de la mano, visualizar cartas de la mano,...
-*/
+ */
 package com.uvigo.poyectosushigo.CORE;
+import com.uvigo.poyectosushigo.CORE.carta.*;
+public class Mano {
 
+    private int numCartas;
+    private Carta[] cartasMano;
 
-public class Mano 
-{
-   
-
+    public Mano(int numCartas, Carta[] cartasMano) {
+        this.numCartas = numCartas;
+        this.cartasMano = cartasMano;
+    }
 
 }
