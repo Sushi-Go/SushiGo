2fc86ca9c3eca3fa63706f4455ea7056353fcde3
Daviiiiid123 <61392628+Daviiiiid123@users.noreply.github.com>
2021-05-05 09:13:14 +0200

    Update ES.java

--- ProySushiGo/src/com/uvigo/proyectosushigo/IU/ES.java
+++ ProySushiGo/src/com/uvigo/proyectosushigo/IU/ES.java
@@ -1,35 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.uvigo.proyectosushigo.IU;
 
 import java.util.Scanner;
 
+public class ES {
 
-public class ES
-{
     public static Scanner leer = new Scanner(System.in);
-    
-    public static String pideCadena(String mensaje)
-    {
-         // Poner el mensaje
-            System.out.println(mensaje);
-               
-             // Pedir
-            return leer.nextLine();
-               
-    }
-    
-        
-    public static int pideNumero(String mensaje)
-    {
-        
-            System.out.println(mensaje);
 
-            // Pedir
-            return Integer.parseInt(leer.nextLine());
-       
+    public static String pideCadena(String mensaje) {
+        // Poner el mensaje
+        System.out.println(mensaje);
+
+        // Pedir
+        return leer.nextLine();
+
     }
+
+    public static int pideNumero(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        boolean esValido = false; // True: entero leido correctamente
+        int leer = 0;
+
+        do {
+            try {
+                System.out.print(mensaje);
+                leer = Integer.parseInt(scanner.nextLine().trim());
+                esValido = true;
+            } catch (NumberFormatException e) {
+                System.err.println("La cadena introducida no se puede "
+                        + "convertir a número entero. Por favor, "
+                        + "introdúcela de nuevo.");
+            }
+        } while (!esValido);
+
+        return leer;
+    }
+
+    private static double pideReal(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        boolean esValido = false; // True: entero leido correctamente
+        double leer = 0;
+
+        do {
+            try {
+                System.out.print(mensaje);
+                leer = Double.parseDouble(scanner.nextLine().trim());
+                esValido = true;
+            } catch (NumberFormatException e) {
+                System.err.println("La cadena introducida no se puede "
+                        + "convertir a número real. Por favor, "
+                        + "introdúcela de nuevo.");
+            }
+        } while (!esValido);
+
+        return leer;
+    }
+
+    private static char pideCaracter(String msg) {
+        return pideCadena(msg).toUpperCase().charAt(0);
+    }
+
 }
