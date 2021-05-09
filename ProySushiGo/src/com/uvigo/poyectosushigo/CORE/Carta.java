81264a4bf3373c7d644b5b6616ea2c9bc5f2e9ca
Lookos <78797521+LookosZero@users.noreply.github.com>
2021-05-09 20:06:43 +0200

    Update Carta.java

--- ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Carta.java
+++ ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Carta.java
@@ -4,19 +4,32 @@
 package com.uvigo.poyectosushigo.CORE;
 
 public class Carta {
-    private String nombre;
-    
-    public Carta(String nombre){
-        this.nombre=nombre;
+
+    private final String nombre;
+
+    /**
+     * Crea una nueva carta
+     *
+     * @param nombre nombre de la carta
+     */
+    public Carta(String nombre) {
+        this.nombre = nombre;
     }
-    
-    public String getNombre(){
+
+    /**
+     * Devuelve el nombre de la carta
+     *
+     * @return el nombre de la carta, como String
+     */
+    public String getNombre() {
         return nombre;
     }
-    
+
     @Override
-    public String toString(){
-        return "[" + getNombre() + "]";
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[").append(nombre).append("]");
+        return sb.toString();
     }
-	
+
 }
