3b20b9097f341f6b3943b17989ebe8bea4708eeb
JesusDiaguez <83415657+JesusDiaguez@users.noreply.github.com>
2021-05-09 18:47:05 +0200

    Update Baraja.java

--- ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Baraja.java
+++ ProySushiGo/src/com/uvigo/poyectosushigo/CORE/Baraja.java
@@ -1,98 +1,76 @@
 package com.uvigo.poyectosushigo.CORE;
 
-import pila.*;
-import lista.*;
-import com.uvigo.poyectosushigo.CORE.carta.*;
+import pila.EnlazadaPila;
+import pila.Pila;
 
 public class Baraja {
-
-    private Pila<Carta> baraja;
-    private int numCartas;
-
+    
+    private final Pila<Carta> baraja;
+    
+    /**
+     * Crea una baraja con todas las cartas
+     */
     public Baraja() {
-        numCartas = 0;
         baraja = new EnlazadaPila<>();
-        //Creamos las 5 cartas de nigiri de calamar
-        for (int i = 1; i <= 5; i++) {
-            baraja.push(new NigiriCalamar());
-            numCartas++;
-        }
-        //Creamos las 10 cartas de nigiri de salmon
-        for (int i = 1; i <= 10; i++) {
-            baraja.push(new NigiriSalmon());
-            numCartas++;
-        }
-        //Creamos las 5 cartas de nigiri de tortilla
-        for (int i = 1; i <= 5; i++) {
-            baraja.push(new NigiriTortilla());
-            numCartas++;
-        }
-        //Creamos las 6 cartas de wasabi
-        for (int i = 1; i <= 6; i++) {
-            baraja.push(new Wasabi());
-            numCartas++;
-        }
-        //Creamos las 6 cartas de maki de 1 rollo
-        for (int i = 1; i <= 6; i++) {
-            baraja.push(new Maki(1));
-            numCartas++;
-        }
-        //Creamos las 12 cartas de maki de 2 rollos
-        for (int i = 1; i <= 12; i++) {
-            baraja.push(new Maki(2));
-            numCartas++;
-        }
-        //Creamos las 8 cartas de maki de 3 rollos
-        for (int i = 1; i <= 8; i++) {
-            baraja.push(new Maki(3));
-            numCartas++;
-        }
-        //Creamos las 14 cartas de tempura
-        for (int i = 1; i <= 14; i++) {
-            baraja.push(new Tempura());
-            numCartas++;
-        }
-        //Creamos las 14 cartas de sashimi
-        for (int i = 1; i <= 14; i++) {
-            baraja.push(new Sashimi());
-            numCartas++;
-        }
-        //Creamos las 14 cartas de gyoza
-        for (int i = 1; i <= 14; i++) {
-            baraja.push(new Gyoza());
-            numCartas++;
-        }
+        addCartas("Nigiri de calamar", 5);
+        addCartas("Nigiri de salmón", 10);
+        addCartas("Nigiri de tortilla", 5);
+        addCartas("Wasabi", 6);
+        addCartas("Maki de 1 rollo", 6);
+        addCartas("Maki de 2 rollos", 12);
+        addCartas("Maki de 3 rollos", 8);
+        addCartas("Tempura", 14);
+        addCartas("Sashimi", 14);
+        addCartas("Gyoza", 14);
     }
-
-    public void barajar() {
-            Pila<Carta> aux1=null;//Crea dos pilas auxiliares
-            Pila<Carta> aux2=null;
-            //Se repite varias veces para aumentar la aleatoriedad
-            for(int i=0; i<10;i++){
-            //mientras que la baraja no este vacia:
-            //introduce aleatoriamente los elementos en dos arrays
-            while(!baraja.esVacio()){
-                 int aleatorio= (int)Math.random()*10; 
-                 if(aleatorio<5){
-                     aux1.push(baraja.pop());
-                 }
-                 else{
-                     aux2.push(baraja.pop());
-                 }
-            }
-            //se devuelven a la pila principal
-            while(!aux1.esVacio()){
-                baraja.push(aux1.pop());
-            }
-            while(!aux2.esVacio()){
-                baraja.push(aux2.pop());
-            }
-            }
+    
+    /**
+     * Devuelve el número de cartas de la baraja
+     * 
+     * @return el número de cartas, como int
+     */
+    public int numCartas() {
+        return baraja.tamaño();
     }
-
-    public Carta darCarta() {
+    
+    /**
+     * Devuelve la primera carta de la baraja
+     * 
+     * @return la primera Carta de la pila
+     */
+    public Carta quitarCarta() {
         return baraja.pop();
-        //Devuelve la primera carta en el mazo
     }
+    
+    /**
+     * Reordena la baraja de forma aleatoria
+     */
+    public void barajar() {
+        int numCartas = numCartas();
+        Carta[] aux = new Carta[numCartas];
+        int random;
 
+        for (int i = 0; !baraja.esVacio(); i++) {
+            aux[i] = quitarCarta();
+        }
+
+        for (int i = numCartas - 1; i >= 0; i--) {
+            random = (int) (Math.random() * i);
+            baraja.push(aux[random]);
+            aux[random] = aux[i];
+        }
+    }
+    
+    /**
+     * Añade varias cartas a la baraja
+     * 
+     * @param nombre nombre de las cartas
+     * @param cantidad número de cartas a añadir
+     */
+    private void addCartas(String nombre, int cantidad) {
+        for (int i = 0; i < cantidad; i++) {
+            baraja.push(new Carta(nombre));
+        }
+    }
+    
 }
